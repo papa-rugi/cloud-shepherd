d25d6ab2bafbc796dca0b0c9ef2e9f17f682c761
Alexander Long <alexanderlcodes@gmail.com>
2018-07-08 15:37:38 -0700

    Latest push for starbucks guy

--- test/test.js
+++ test/test.js
@@ -1,46 +1,16 @@
-'use strict';
 
-var expect = require('chai').expect;
-var numFormatter = require('../index');
 
-describe('#numFormatter', function() {
-    it('should convert single digits', function() {
-        var result = numFormatter(1);
-        expect(result).to.equal('1');
-    });
 
-    it('should convert double digits', function() {
-        var result = numFormatter(12);
-        expect(result).to.equal('12');
-    });
+const cloudherder = require('../lib/main.js');
 
-    it('should convert triple digits', function() {
-        var result = numFormatter(123);
-        expect(result).to.equal('123');
-    });
+const credentials = require('./credentials');
 
-    it('should convert 4 digits', function() {
-        var result = numFormatter(1234);
-        expect(result).to.equal('1,234');
-    });
+const source = cloudherder.cloudFactory.issue('s3', credentials);
 
-    it('should convert 5 digits', function() {
-        var result = numFormatter(12345);
-        expect(result).to.equal('12,345');
-    });
 
-    it('should convert 6 digits', function() {
-        var result = numFormatter(123456);
-        expect(result).to.equal('123,456');
-    });
 
-    it('should convert 7 digits', function() {
-        var result = numFormatter(1234567);
-        expect(result).to.equal('1,234,567');
-    });
 
-    it('should convert 8 digits', function() {
-        var result = numFormatter(12345678);
-        expect(result).to.equal('12,345,678');
-    });
-});
+
+
+
+cloudherder.s3Cloud.test();
